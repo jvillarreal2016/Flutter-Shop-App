21ad26f32fc531a8a7a48a675893587d331e31a1
Javi Villarreal <javivillarreal@Javis-MacBook-Pro.local>
2021-10-13 11:02:06 -0500

    finished module

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import './screens/user_products_screen.dart';
 import './screens/edit_products_screen.dart';
 import './screens/auth_screen.dart';
 import './providers/auth.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -52,6 +53,11 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.purple,
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                },
+              ),
             ),
             home: auth.isAuth
                 ? ProductsOverviewScreen()
